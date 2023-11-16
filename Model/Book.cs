adae680962faee861c42d96a09e32702a06c0f13
NikolaevMD <NikolaevMD@410D-2>
2023-11-16 15:02:32 +0700

    Добавьте файлы проекта.

---
+++ Model/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMVVM_Application.Model
+{
+    public class Book
+    {
+
+        public Book(string author, string name, short arc, int age, int count)
+        {
+            Author = author;
+            Name = name;
+            Arc = arc;
+            Age = age;
+            Count = count;
+        }
+
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public short Arc { get; set; }
+        public int Age { get; set; }
+        public int Count { get; set; }
+
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMVVM_Application.Model
+{
+    public class User
+    {
+        public User(int id, string name, string surname, List<Book> BookList)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            this.BookList = BookList;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public List<Book> BookList { get; set; }
+    }
+}
